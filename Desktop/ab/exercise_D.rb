3ab2ad061d877ea3e6a3b044511890c6686f2596
SINAN ORION CANSELEN <orion@sinans-mbp.ait-sydney.net.au>
2018-08-27 08:47:35 +1000

    rate equation

---
+++ Desktop/ab/exercise_D.rb
@@ -0,0 +1,13 @@
+b = 3 + 4 + 7
+c = b/3.0
+print c
+# bank-rate equation
+
+hours = 80
+rate = 1.43
+money = 143
+if hours <= 40 
+    puts money
+else 
+    puts money * rate + (hours - 40) * rate * 1.5
+end
