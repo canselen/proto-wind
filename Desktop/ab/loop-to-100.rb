655bd623d423a03db8f61e85845a00354d6edb69
SINAN ORION CANSELEN <orion@sinans-mbp.ait-sydney.net.au>
2018-08-27 17:03:37 +1000

    27.08.18 challange

---
+++ Desktop/ab/loop-to-100.rb
@@ -0,0 +1,20 @@
+
+#first code challange
+
+d_number = 0
+
+while d_number < 100 do
+ d_number = d_number + 1
+ puts d_number
+end 
+
+# second code challange
+
+false_dogN = "2"
+false_dogR = "german shaperd and pitbul"
+true_dogN= "3"
+true_dogR= "2 german shaperd and a rescue"
+time_of_day="afternoon"
+print "Hi I am sinan I own #{false_dogN} dog, a #{false_dogR} to be presize... "
+puts "Pardon me I must have work over hours  I meant #{true_dogN} which are #{true_dogR}" 
+puts "Well lads,#{time_of_day} , see you later."
